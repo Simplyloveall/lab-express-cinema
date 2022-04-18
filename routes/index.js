6dfef2cd4eaa2a6efd6791aa248484b32fb5568b
Simply Loveall <max.hkre@gmail.com>
2022-04-18 15:59:40 -0400

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then(function (allMovies) {
+      console.log("Found movies", allMovies);
+      res.render("movies", { allMovies: allMovies });
+    })
+    .catch(function (err) {
+      console.log("Something went wrong", err.message);
+    });
+});
+
+router.get("/movies/:movieId", (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .then(function (movie) {
+      console.log("Found movies", movie);
+      res.render("movie-details", { theMovie: movie });
+    })
+    .catch(function (err) {
+      console.log("Something went wrong", err.message);
+    });
+});
 
 module.exports = router;
